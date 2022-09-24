5a5db9465dde7ce7d62035f82084b8208ee73230
jon-michael-c <jonmichaelcubano@gmail.com>
2022-09-23 22:19:04 -0400

    changed port to 3333

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 3000;
+const PORT = 3333;
 const mongoose = require("mongoose");
 const UserModel = require("./models/Users");
 const RecipeModel = require("./models/Recipe");
